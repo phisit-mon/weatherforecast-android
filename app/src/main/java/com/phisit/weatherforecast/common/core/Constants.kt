6bf13f5f9bc704a5608af2e2b3b807a810374e5a
Phisit Mongkolnitayakul <phisit.mon@gmail.com>
2022-05-31 01:06:12 +0700

    - Setup source files ,fonts, styles , strings , xml layout , color

---
+++ app/src/main/java/com/phisit/weatherforecast/common/core/Constants.kt
@@ -0,0 +1,8 @@
+package com.phisit.weatherforecast.common.core
+
+object Constants {
+    // Celsius
+    const val TEMP_UNIT_C = "metric"
+    // Fahrenheit
+    const val TEMP_UNIT_F = "metric"
+}
